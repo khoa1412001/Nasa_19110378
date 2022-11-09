38803552b7ca8a2e47c2a264938b190d11530bcd
Khoa Dang <khoa9205141@gmail.com>
2022-11-09 16:04:07 +0700

    testcase

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const request = require("supertest");
 
 const app = require("./app");
 
@@ -13,7 +14,87 @@ async function startServer() {
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
+
+    startTest();
   });
 }
 
 startServer();
+
+function startTest() {
+  //testcase 1
+  request(app)
+    .get("/planets")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .end(function (err, res) {
+      if (err) console.log(err.message);
+    });
+  //testcase 2
+  request(app)
+    .get("/launches")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .end(function (err, res) {
+      if (err) console.log(err.message);
+    });
+  //testcase 3
+  request(app)
+    .post("/launches")
+    .send({
+      mission: "Testcase",
+      rocket: "Rocket 101",
+      launchDate: "01/13/2023",
+      target: "Kepler 60 b",
+    })
+    .expect("Content-Type", /json/)
+    .expect(201)
+    .end(function (err, res) {
+      if (err) console.log(err.message);
+    });
+  //test case 4
+  request(app)
+    .delete("/launches/100")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .end(function (err, res) {
+      if (err) console.log(err.message);
+    });
+  //test case 5
+  request(app)
+    .delete("/launches/105")
+    .expect("Content-Type", /json/)
+    .expect(404)
+    .end(function (err, res) {
+      if (err) console.log(err.message);
+    });
+  //test case 6
+  request(app)
+    .post("/launches")
+    .send({
+      mission: "Testcase",
+      rocket: "Rocket 101",
+      target: "Kepler 60 b",
+    })
+    .expect("Content-Type", /json/)
+    .expect(400)
+    .end(function (err, res) {
+      if (err) console.log(err.message);
+    });
+  //testcase 7
+  request(app)
+    .get("/planets/1")
+    .expect("Content-Type", /json/)
+    .expect(400)
+    .end(function (err, res) {
+      if (err) console.log(err.message);
+    });
+  //testcase 8
+  request(app)
+    .get("/launches/1")
+    .expect("Content-Type", /json/)
+    .expect(400)
+    .end(function (err, res) {
+      if (err) console.log(err.message);
+    });
+}
